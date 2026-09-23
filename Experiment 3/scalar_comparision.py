d39430711c51fb2813280d64f7110c24e34689ef
Anjali Kumari <anjalikumariakri08@gmail.com>
2026-09-23 18:04:24 +0530

    Update scalar_comparision.py

--- Experiment 3/scalar_comparision.py
+++ Experiment 3/scalar_comparision.py
@@ -1,7 +1,5 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
-# Sample data
 data = np.array([
     [20, 25000],
     [21, 30000],
@@ -9,20 +7,14 @@ data = np.array([
     [23, 35000],
     [24, 40000]
 ])
-
-# StandardScaler
 standard = StandardScaler()
 standard_data = standard.fit_transform(data)
-
 print("--- StandardScaler ---")
 print(standard_data)
 print("Minimum:", standard_data.min())
 print("Maximum:", standard_data.max())
-
-# MinMaxScaler
 minmax = MinMaxScaler()
 minmax_data = minmax.fit_transform(data)
-
 print("\n--- MinMaxScaler ---")
 print(minmax_data)
 print("Minimum:", minmax_data.min())
